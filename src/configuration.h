68820d66c6bf6849d6b00456d142bdaa30d28e6b
BartelKarlijn <67980453+BartelKarlijn@users.noreply.github.com>
2020-09-17 16:55:41 +0200

    volgorde stripstuks aanpassen

--- src/configuration.h
+++ src/configuration.h
@@ -38,39 +38,38 @@ byte stopachter[3] ;           // 0=van, 1=tot, 2=num
 
 // pointers om volgorde aan te geven.  pointer1 = 1e stuk vd led strip
 // helaas weet ik niet hoe je naar array pointers kan wijzen
-byte *pointer1v = &mainacht_r[0];
-byte *pointer1t = &mainacht_r[1];
-byte *pointer1n = &mainacht_r[2];
+byte *pointer1v = &mainrond_l[0];
+byte *pointer1t = &mainrond_l[1];
+byte *pointer1n = &mainrond_l[2];
 
-byte *pointer2v = &mainrond_r[0];
-byte *pointer2t = &mainrond_r[1];
-byte *pointer2n = &mainrond_r[2];
+byte *pointer2v = &drl_voor_l[0];
+byte *pointer2t = &drl_voor_l[1];
+byte *pointer2n = &drl_voor_l[2];
 
-byte *pointer3v = &drl_voor_l[0];
-byte *pointer3t = &drl_voor_l[1];
-byte *pointer3n = &drl_voor_l[2];
+byte *pointer3v = &drl_voor_r[0];
+byte *pointer3t = &drl_voor_r[1];
+byte *pointer3n = &drl_voor_r[2];
 
-byte *pointer4v = &drl_voor_r[0];
-byte *pointer4t = &drl_voor_r[1];
-byte *pointer4n = &drl_voor_r[2];
+byte *pointer4v = &mainrond_r[0];
+byte *pointer4t = &mainrond_r[1];
+byte *pointer4n = &mainrond_r[2];
 
-byte *pointer5v = &drl_acht_l[0];
-byte *pointer5t = &drl_acht_l[1];
-byte *pointer5n = &drl_acht_l[2];
+byte *pointer5v = &mainacht_r[0];
+byte *pointer5t = &mainacht_r[1];
+byte *pointer5n = &mainacht_r[2];
 
-byte *pointer6v = &mainacht_l[0];
-byte *pointer6t = &mainacht_l[1];
-byte *pointer6n = &mainacht_l[2];
+byte *pointer6v = &drl_acht_r[0];
+byte *pointer6t = &drl_acht_r[1];
+byte *pointer6n = &drl_acht_r[2];
 
-byte *pointer7v = &drl_acht_r[0];
-byte *pointer7t = &drl_acht_r[1];
-byte *pointer7n = &drl_acht_r[2];
+byte *pointer7v = &mainacht_l[0];
+byte *pointer7t = &mainacht_l[1];
+byte *pointer7n = &mainacht_l[2];
 
-byte *pointer8v = &mainrond_l[0];
-byte *pointer8t = &mainrond_l[1];
-byte *pointer8n = &mainrond_l[2];
+byte *pointer8v = &drl_acht_l[0];
+byte *pointer8t = &drl_acht_l[1];
+byte *pointer8n = &drl_acht_l[2];
 
 byte *pointer9v = &stopachter[0];
 byte *pointer9t = &stopachter[1];
 byte *pointer9n = &stopachter[2];
-
