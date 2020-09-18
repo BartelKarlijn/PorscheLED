51c883c7fc9dc2d4caa6b174d08457c8eaff9a36
BartelKarlijn <67980453+BartelKarlijn@users.noreply.github.com>
2020-09-18 20:20:08 +0200

    compacter left align html buttons

--- src/wifi/htmlbutton.h
+++ src/wifi/htmlbutton.h
@@ -1,15 +1,16 @@
 String htmlbutton( bool status, String omschrijving, String handlecode) {
   String ptr = "<p>";
-  ptr += omschrijving ;
   if (status) {
-    ptr += "</p><a class=\"button button-of\" href=\"/" ; 
+    ptr += "<a class=\"button button-of\" href=\"/" ; 
     ptr += handlecode;
-    ptr += "\">ON</a>\n" ;
+    ptr += "\">ON  </a>\n" ;
     }
   else {
-    ptr += "</p><a class=\"button button-on\" href=\"/" ; 
+    ptr += "<a class=\"button button-on\" href=\"/" ; 
     ptr += handlecode;
     ptr += "\">OFF</a>\n" ;
   }
+  ptr += omschrijving;
+  ptr += "</p>\n";
   return ptr;
 }
--- src/wifi/sendhtml.h
+++ src/wifi/sendhtml.h
@@ -4,14 +4,16 @@ String SendHTML(){
   // Title
   ptr +="<title>Porsche LED</title>\n";
   // CSS to style the on/off buttons 
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 50px;background-color: #3498db;border: none;color: white;padding: 6px 10px;text-decoration: none;font-size: 18px;margin: 0px auto 25px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: left;}\n";
+  ptr +="body{margin-top: 50px;}\n";
+  ptr +="h1 {color: #444444;margin: 50px auto 30px; text-align: center;}\n";
+  ptr +="h3 {color: #444444;margin-bottom: 50px; text-align: center;}\n";
+  ptr +=".button {display: inline;width: 60px;background-color: #3498db;border: none;color: white;padding: 10px 10px;text-decoration: none;font-size: 25px;margin: 10px auto 25px;cursor: pointer;border-radius: 4px; margin-bottom: 50px;}\n";
   ptr +=".button-on {background-color: #3498db;}\n";
   ptr +=".button-on:active {background-color: #2980b9;}\n";
   ptr +=".button-of {background-color: #34495e;}\n";
   ptr +=".button-of:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="p {font-size: 20px;color: #888;margin-bottom: 30px;}\n";
   ptr +="</style>\n";
   ptr +="</head>\n";
 // bodytitel
@@ -29,6 +31,6 @@ String SendHTML(){
 
   ptr +="</body>\n";
   ptr +="</html>\n";
-  //printdebug(ptr);
+  printdebug(ptr);
   return ptr;
 }
